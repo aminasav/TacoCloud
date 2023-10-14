991545b89b9e1270a10f11b70304231d339c2380
aminasav <aminat.magomadova27@gmail.com>
2023-10-14 16:44:12 +0300

    Initial commit

---
+++ src/main/java/com/example/springinaction/tacoapp/TacoOrder.java
@@ -0,0 +1,34 @@
+package com.example.springinaction.tacoapp;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+    @NotBlank(message = "Delivery name is required")
+    public String deliveryName;
+    @NotBlank(message = "Delivery street is required")
+    public String deliveryStreet;
+    @NotBlank(message = "Delivery city is required")
+    public String deliveryCity;
+    @NotBlank(message = "State  is required")
+    public String deliveryState;
+    @NotBlank(message = "Zip is required")
+    public String deliveryZip;
+    @CreditCardNumber(message = "Not a valid credit card number")
+    public String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\ /])([2-9][0-9])$",
+            message="Must be formatted MM/YY")
+    public String ccExpiration;
+    @Digits(integer =3, fraction = 0, message = "Invalid CVV")
+    public String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+    public void addTaco(Taco taco){
+        this.tacos.add(taco);
+    }
+}
---
+++ src/main/java/com/example/springinaction/tacoapp/controllers/OrderController.java
@@ -0,0 +1,31 @@
+package com.example.springinaction.tacoapp.controllers;
+
+import com.example.springinaction.tacoapp.Taco;
+import com.example.springinaction.tacoapp.TacoOrder;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(){
+        return "orderForm";
+    }
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors,
+                               SessionStatus sessionStatus)
+    {
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/"; }
+}
